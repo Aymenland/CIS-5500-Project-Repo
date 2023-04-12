0cd4de0dc1fed529f84e3a4e14db9b67ab7e11b0
candylyt <candyliu.lyt@gmail.com>
2023-04-11 23:44:24 -0400

    top-reviewers basic ui done

--- books-database/src/App.js
+++ books-database/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 
@@ -6,13 +7,12 @@ import BookSearch from './pages/BookSearch';
 import BookRecommendation from './pages/BookRecommendation';
 import TopPublishers from './pages/TopPublishers';
 import TopReviewers from './pages/TopReviewers';
-import Header from './components/header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
         <Routes>
           <Route path="/" element={<BookSearch/>}/>
           <Route path="/search" element={<BookSearch/>}/>
@@ -20,6 +20,7 @@ function App() {
           <Route path="/toppublishers" element={<TopPublishers/>}/>
           <Route path="/topreviewers" element={<TopReviewers/>}/>
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- books-database/src/pages/TopReviewers.js
+++ books-database/src/pages/TopReviewers.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import Pagination from '../components/Pagination';
+import './TopReviewers.css';
 
 function TopReviewers() {
   return(
-    <div>
-      Top Reviewers Page
+    <div className="container">
+      <h1 className="review-header">
+        Top Reviewers
+      </h1>
+      <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Age</th>
+          <th scope="col">Location</th>
+          <th scope="col">Number of Books Rated</th>
+          <th scope="col">Average Ratings</th>
+          <th scope="col">Highest Rating Given</th>
+          <th scope="col">Lowest Rating Given</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">34</th>
+          <td>New York, United States</td>
+          <td>3920</td>
+          <td>7</td>
+          <td>10</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <th scope="row">23</th>
+          <td>California, United States</td>
+          <td>3913</td>
+          <td>7</td>
+          <td>10</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <th scope="row">56</th>
+          <td>New York, United States</td>
+          <td>2940</td>
+          <td>5</td>
+          <td>10</td>
+          <td>3</td>
+        </tr>
+      </tbody>
+      </table>
+      <Pagination/>
     </div>
   )
 }
