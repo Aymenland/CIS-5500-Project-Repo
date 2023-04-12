913129565744f8b8f134be0c616777433ed74b7d
jtherm <117696705+jtherm@users.noreply.github.com>
2023-04-12 19:15:27 +0000

    initial changes to the TopPublishers page

--- books-database/src/pages/TopPublishers.js
+++ books-database/src/pages/TopPublishers.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TopPublishers() {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(15);
+
+  const indexOfLastRow = currentPage * rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+
+
   return(
-    <div>
-      Top Publishers Page
+    <div className="container">      
+      <h1 className="publisher-header">
+      Top Publishers
+      </h1>
+      <table className="table table-striped">
+      <thead>
+        <tr>
+        <th scope="col">ID</th>
+          <th scope="col">Publisher</th>
+          <th scope="col">Number of Books Released</th>
+          </tr>
+      </thead>
+      <tbody>
+        ...
+      </tbody>
+      </table>
+      
     </div>
   )
 }
