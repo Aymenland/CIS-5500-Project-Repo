bff049bad8479f26a4a5e6543f3dd18a6724776c
jtherm <117696705+jtherm@users.noreply.github.com>
2023-04-30 19:09:46 -0400

    Update TopPublishers.js
    updated page reference

--- books-database/src/pages/TopPublishers.js
+++ books-database/src/pages/TopPublishers.js
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './TopPublishers.css';
+const config = require('../config.json')
 
 function TopPublishers() {
-
   const [currentPage, setCurrentPage] = useState(1);
-  const [rowsPerPage, setRowsPerPage] = useState(15);
+  const rowsPerPage = 10;
+  const [dbResult, setDbResult] = useState([]);
 
   const indexOfLastRow = currentPage * rowsPerPage;
   const indexOfFirstRow = indexOfLastRow - rowsPerPage;
 
+  let currentRows;
+  let publisherElement;
 
+  useEffect(() => {
+    fetch(`http://${config.server_host}:${config.server_port}/top_publishers`)
+    .then(res => res.json())
+    .then(resJson => setDbResult(resJson.data));
+  })
+  if (dbResult) {
+    currentRows = dbResult.slice(indexOfFirstRow, indexOfLastRow);
+    publisherElement = currentRows.map(publisher => {
+      return(
+        <tr key={publisher.Publisher}>
+          <th scope="row">{publisher.Publisher}</th>
+          <td>{publisher.Avg_Publisher_Rating}</td>
+          <td>{publisher.Number_Of_Reviewers}</td>
+      </tr>
+      )
+    })
+  }
   return(
     <div className="container">      
       <h1 className="publisher-header">
-      Top Publishers
+        Top Publishers
       </h1>
       <table className="table table-striped">
       <thead>
         <tr>
-        <th scope="col">ID</th>
-          <th scope="col">Publisher</th>
-          <th scope="col">Number of Books Released</th>
+        <th scope="col">Publisher</th>
+          <th scope="col">Avg_Publisher_Rating</th>
+          <th scope="col">Number_Of_Reviewers</th>
           </tr>
       </thead>
       <tbody>
-        <tr>
-        <td>...</td>
-        </tr>
+        {publisherElement}
       </tbody>
       </table>
       
